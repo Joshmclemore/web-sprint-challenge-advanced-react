ceefadede71336ff951f4f10a87d8cb81eb5e32f
Joshmclemore <josh_mclemore@yahoo.com>
2022-03-11 12:20:48 -0600

    Class directional buttons and working and limited correctly with error messages

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -4,22 +4,70 @@ import axios from 'axios';
 const URL = 'http://localhost:9000/api/result';
 
 const initialState = {
-  x: "",
-  y: "", 
-  steps: "", 
-  email: "",
+  x: 2,
+  y: 2,
+  matrix: [
+    [0,0,0],
+    [0,1,0],
+    [0,0,0],
+  ],
+  errorMessage: ""
 }
 
 export default class AppClass extends React.Component {
 
   state = initialState;
 
+  moveRight = () => {
+
+    let counter = this.state.x + 1;
+
+    if(this.state.x < 3){
+      this.setState({...this.state, x: counter})}
+    else {
+      this.setState({...this.state, errorMessage: "You can't go right"})
+    }
+  }
+
+  moveLeft = () => {
+
+    let counter = this.state.x - 1;
+
+    if(this.state.x > 1){
+      this.setState({...this.state, x: counter})}
+    else {
+      this.setState({...this.state, errorMessage: "You can't go left"})
+    }
+  }
+
+  moveUp = () => {
+
+    let counter = this.state.y + 1;
+
+    if(this.state.y < 3){
+      this.setState({...this.state, y: counter})}
+    else {
+      this.setState({...this.state, errorMessage: "You can't go up"})
+    }
+  }
+
+  moveDown = () => {
+
+    let counter = this.state.y - 1;
+
+    if(this.state.y > 1){
+      this.setState({...this.state, y: counter})}
+    else {
+      this.setState({...this.state, errorMessage: "You can't go down"})
+    }
+  }
+
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
+          <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
           <h3 id="steps">You moved 0 times</h3>
         </div>
         <div id="grid">
@@ -34,13 +82,13 @@ export default class AppClass extends React.Component {
           <div className="square"></div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.errorMessage}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
+          <button id="left" onClick={() => this.moveLeft()}>LEFT</button>
+          <button id="up"onClick={() => this.moveUp()}>UP</button>
+          <button id="right" onClick={() => this.moveRight()}>RIGHT</button>
+          <button id="down"onClick={() => this.moveDown()}>DOWN</button>
           <button id="reset">reset</button>
         </div>
         <form>
