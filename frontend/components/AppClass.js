ac7be7486023ab455021b6e280c8ab75c956de2e
Joshmclemore <josh_mclemore@yahoo.com>
2022-03-11 17:42:25 -0600

    submit is functioning and displaying a success message

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -6,28 +6,39 @@ const URL = 'http://localhost:9000/api/result';
 const initialState = {
   x: 2,
   y: 2,
-  matrix: [
-    [0,0,0],
-    [0,1,0],
-    [0,0,0],
-  ],
   steps: 0,
-  errorMessage: ""
+  errorMessage: "",
+  successMessage: "",
+  email: "",
+  grid: [
+    null, null, null,
+    null, "B", null,
+    null, null, null ],
 }
 
-// { "x": 1, "y": 2, "steps": 3, "email": "lady@gaga.com" }
+
 
 export default class AppClass extends React.Component {
 
   state = initialState;
 
+
+  // setGridLocation = () => {
+  //   if(this.state.x === 2 && this.state.y === 2){
+  //     console.log(this.state.grid[4])
+  //     this.setState({...this.state, grid: [this.state.grid[4], "B"]})
+  //   } else {
+  //     console.log('try again')
+  //   }
+  // }
+  
   moveRight = () => {
 
     let xCounter = this.state.x + 1;
     let stepsCounter = this.state.steps + 1;
 
     if(this.state.x < 3){
-      this.setState({...this.state, x: xCounter, steps: stepsCounter})}
+      this.setState({...this.state, x: xCounter, steps: stepsCounter, errorMessage: ""})}
     else {
       this.setState({...this.state, errorMessage: "You can't go right"})
     }
@@ -39,38 +50,77 @@ export default class AppClass extends React.Component {
     let stepsCounter = this.state.steps + 1;
 
     if(this.state.x > 1){
-      this.setState({...this.state, x: xCounter, steps: stepsCounter})}
+      this.setState({...this.state, x: xCounter, steps: stepsCounter, errorMessage: ""})}
     else {
       this.setState({...this.state, errorMessage: "You can't go left"})
     }
   }
 
-  moveUp = () => {
+  moveDown = () => {
 
     let yCounter = this.state.y + 1;
     let stepsCounter = this.state.steps + 1;
 
     if(this.state.y < 3){
-      this.setState({...this.state, y: yCounter, steps: stepsCounter})}
-    else {
-      this.setState({...this.state, errorMessage: "You can't go up"})
-    }
-  }
-
-  moveDown = () => {
-
-    let yCounter = this.state.y - 1;
-    let stepsCounter = this.state.steps + 1;
-
-    if(this.state.y > 1){
-      this.setState({...this.state, y: yCounter, steps: stepsCounter})}
+      this.setState({...this.state, y: yCounter, steps: stepsCounter, errorMessage: ""})}
     else {
       this.setState({...this.state, errorMessage: "You can't go down"})
     }
   }
 
+  moveUp = () => {
+
+    let yCounter = this.state.y - 1;
+    let stepsCounter = this.state.steps + 1;
+    
+    if(this.state.y > 1){
+      this.setState({...this.state, y: yCounter, steps: stepsCounter, errorMessage: ""})
+      window.localStorage.key('cat', 'dog')}
+    else {
+      this.setState({...this.state, errorMessage: "You can't go up"})
+    }
+  }
+
+
+
+  //onSubmit will be axios.post(url) and return a success or error message
+
+  //reset will set state back to initial state
+
+  reset = () => {
+    this.setState(initialState)
+  }
+
+  changeInput = (evt) => {
+    const { key, value } = evt.target
+    this.setState({
+      ...this.state,
+      email: evt.target.value 
+    })
+  }
+
+  onSubmit = (e) => {
+    const newInfo = {
+      "x": this.state.x,
+      "y": this.state.y,
+      "steps": this.state.steps,
+      "email": this.state.email,
+    }
+    e.preventDefault()
+    axios.post(URL, newInfo)
+    .then(res => {
+      console.log(res)
+      this.setState({
+        ...this.state,
+        successMessage: [...this.state.successMessage, res.data.message]
+      })
+    })
+  }
+// { "x": 1, "y": 2, "steps": 3, "email": "lady@gaga.com" }
+
   render() {
     const { className } = this.props
+
     return (
       <div id="wrapper" className={className}>
         <div className="info">
@@ -89,17 +139,17 @@ export default class AppClass extends React.Component {
           <div className="square"></div>
         </div>
         <div className="info">
-          <h3 id="message">{this.state.errorMessage}</h3>
+          <h3 id="message">{this.state.errorMessage}{this.state.successMessage}</h3>
         </div>
         <div id="keypad">
           <button id="left" onClick={() => this.moveLeft()}>LEFT</button>
           <button id="up"onClick={() => this.moveUp()}>UP</button>
           <button id="right" onClick={() => this.moveRight()}>RIGHT</button>
           <button id="down"onClick={() => this.moveDown()}>DOWN</button>
-          <button id="reset">reset</button>
+          <button id="reset" onClick={() => this.reset()}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input id="email" type="email" onChange={this.changeInput} placeholder="type email"></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
